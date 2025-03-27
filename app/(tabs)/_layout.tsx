32fea0a91e7f006068f1b07bdcf70b9633ebb764
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-03-27 17:01:32 +0100

    pablo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Tabs } from "expo-router";
 import homeIcon from "@/assets/images/unius.png";
 import transactionIcon from "@/assets/images/transaction.png";
 import earnIcon from "@/assets/images/earn.png";
-import trendingIcon from "@/assets/images/Trending.png";
+// import trendingIcon from "@/assets/images/Trending.png";
 import settingsIcon from "@/assets/images/settings.png";
 
 
@@ -42,16 +42,17 @@ const Layout = () => {
             />
             <Tabs.Screen
                 name="earn"
-                options={{ title: "Earn", headerShown: false, tabBarItemStyle: {paddingTop: 29}, tabBarIcon: ({focused}) => (
-                    <Image source={earnIcon} style={{width: 40, height:40, tintColor: focused? '#0066CC': '#999999' ,}}/>
+                // tabBarItemStyle: {paddingTop: 29},
+                options={{ title: "Earn", headerShown: false, tabBarIcon: ({focused}) => (
+                    <Image source={earnIcon} style={{width: 30, height:26, tintColor: focused? '#0066CC': '#999999' ,}}/>
                 ) }}
             />
-            <Tabs.Screen
+            {/* <Tabs.Screen
                 name="trending"
                 options={{ title: "Trending", headerShown: false, tabBarIcon: ({focused}) => (
                     <Image source={trendingIcon} style={{width: 24, height:26, tintColor: focused? '#0066CC': '#999999'}}/>
                 ) }}
-            />
+            /> */}
             <Tabs.Screen
                 name="settings"
                 options={{ title: "Settings", headerShown: false, tabBarIcon: ({focused}) => (
