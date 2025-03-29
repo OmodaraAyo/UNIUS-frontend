74893057b032c8a14b70540708a0c9bd61744d40
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-03-29 14:50:08 +0100

    pablo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,15 @@
 import { Stack } from "expo-router";
 import "./globals.css";
-import { View } from "react-native";
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="notifications" options={{ headerShown: false,}} />
+      <Stack.Screen name="notifications" options={{ headerShown: false, animation: 'slide_from_right'}} />
       <Stack.Screen name="sendUwt" options={{ headerShown: false }} />
       <Stack.Screen name="otherwallet" options={{ headerShown: false }} />
-      <Stack.Screen name="amount" options={{ headerShown: false }} />
+      <Stack.Screen name="amount" options={{ headerShown: false, animation: 'slide_from_right' }} />
+      <Stack.Screen name="customerCare" options={{ headerShown: false, animation: 'slide_from_right' }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
